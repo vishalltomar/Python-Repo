56e37fdb7b88bc8b728d8e33d61e179095eaae0b
Vishal Tomar <vishaltomar919400@gmail.com>
2026-09-09 00:04:15 +0530

    Add conditional logic for score and age evaluation

---
+++ conditional.py
@@ -0,0 +1,10 @@
+score=35
+result="BT" if score>=10 else "Fail"
+print(result)
+
+age=35
+if age>=18:
+    print("Eligible to vote")
+    pass # yeahh error nhi esliyE use krte hai
+else:
+    print("Not Eligible to vote")
