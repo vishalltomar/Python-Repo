74f759c19400319311c7067e258f3180a92c4455
Vishal Tomar <vishaltomar919400@gmail.com>
2026-09-08 22:29:05 +0530

    Implement list operations in list.py
    Added a Python script to demonstrate list operations including append, sort, insert, count, and slicing.

---
+++ list.py
@@ -0,0 +1,13 @@
+
+  a =[1,2,3] # list of integers
+print(a)
+a.append(19) #append add an element to the end of the list because lis is mutable
+print(a)
+a.sort()#sort method is helpfull to sort thr list in ascending order
+a.insert(1,10) # yeahh method element ko insert krta hasin specific indexx 
+print(a)
+a.append("bannana")
+print(a)
+real=a.count("bannana") #countmethod is used to count the number of letters in the list
+print(real)
+print(a[2:6])
